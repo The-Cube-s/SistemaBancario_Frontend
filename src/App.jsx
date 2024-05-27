6b06f134aa719b9067b8ca4bdfd90a979137b4bc
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-05-27 09:05:46 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-
+import { Login } from './Components/Login/Login'
+import { Register } from './Components/Register/Register'
 
 function App() {
  
 
   return (
     <>
-      
+      <Register/>
     </>
   )
 }

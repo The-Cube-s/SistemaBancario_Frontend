e01c37d34f0769300d1542a1d99ec67daf7930ca
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-05-27 10:44:57 -0600

    Pagina principal con register y login

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,42 +1,42 @@
 import React from 'react'
 
-export const Register = () => {
+export const Register = ({ switchAuthHandler }) => {
   return (
-    <div id="second">
-			      <div class="myform form ">
-                        <div class="logo mb-3">
-                           <div class="col-md-12 text-center">
+    
+			      <div className="myform form position-absolute top-50 start-50 translate-middle ">
+                        <div className="logo mb-3">
+                           <div className="col-md-12 text-center">
                               <h1 >Signup</h1>
                            </div>
                         </div>
                         <form action="#" name="registration"/>
-                           <div class="form-group">
+                           <div className="form-group">
                               <label for="exampleInputEmail1">First Name</label>
-                              <input type="text"  name="firstname" class="form-control" id="firstname" aria-describedby="emailHelp" placeholder="Enter Firstname"/>
+                              <input type="text"  name="firstname" className="form-control" id="firstname" aria-describedby="emailHelp" placeholder="Enter Firstname"/>
                            </div>
-                           <div class="form-group">
+                           <div className="form-group">
                               <label for="exampleInputEmail1">Last Name</label>
-                              <input type="text"  name="lastname" class="form-control" id="lastname" aria-describedby="emailHelp" placeholder="Enter Lastname"/>
+                              <input type="text"  name="lastname" className="form-control" id="lastname" aria-describedby="emailHelp" placeholder="Enter Lastname"/>
                            </div>
-                           <div class="form-group">
+                           <div className="form-group">
                               <label for="exampleInputEmail1">Email address</label>
-                              <input type="email" name="email"  class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
+                              <input type="email" name="email"  className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
                            </div>
-                           <div class="form-group">
+                           <div className="form-group">
                               <label for="exampleInputEmail1">Password</label>
-                              <input type="password" name="password" id="password"  class="form-control" aria-describedby="emailHelp" placeholder="Enter Password"/>
+                              <input type="password" name="password" id="password"  className="form-control" aria-describedby="emailHelp" placeholder="Enter Password"/>
                            </div>
-                           <div class="col-md-12 text-center mb-3">
-                              <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Get Started For Free</button>
+                           <div className="col-md-12 text-center mb-3">
+                              <button type="submit" className=" btn btn-block mybtn btn-primary tx-tfm">Get Started For Free</button>
                            </div>
-                           <div class="col-md-12 ">
-                              <div class="form-group">
-                                 <p class="text-center"><a href="#" id="signin">Already have an account?</a></p>
+                           <div className="col-md-12 ">
+                              <div className="form-group">
+                                 <span className="text-center"><a onClick={switchAuthHandler} id="signin">Already have an account?</a></span>
                               </div>
                            </div>
                             </div>
                         
-                     </div>
+                     
   )
 }
 

cf5b56f181d30d516e34f048e328d0ee37f85e51
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-06-16 22:45:07 -0600

    Front adaptado para imagenes

--- src/Shared/Hooks/Admin/useAddProduct.jsx
+++ src/Shared/Hooks/Admin/useAddProduct.jsx
@@ -5,7 +5,9 @@ export const useAddProduct = () => {
     const [product, setProduct] = useState({
         name: '',
         description: '',
-        price: ''
+        price: '',
+        amount: '', 
+        imagesProduct: [] // Para manejar las imágenes
     });
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
@@ -19,20 +21,37 @@ export const useAddProduct = () => {
         });
     };
 
+    const handleImageChange = (e) => {
+        const files = Array.from(e.target.files);
+        setProduct({
+            ...product,
+            imagesProduct: files,
+        });
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
         setError('');
         setSuccess(false);
 
-        //Por si nos hackean y nos ponen en banca rota jajajajaja
         if (product.price === 0) {
             setError('El precio del producto no puede ser 0');
             setLoading(false);
             return;
         }
+
+        const formData = new FormData();
+        formData.append('name', product.name);
+        formData.append('description', product.description);
+        formData.append('price', product.price);
+        formData.append('amount', product.amount); // Añade el campo `amount`
+        product.imagesProduct.forEach((file, index) => {
+            formData.append('imagesProduct', file);
+        });
+
         try {
-            const response = await addProductRequest(product);
+            const response = await addProductRequest(formData);
             if (response.error) {
                 setError(response.err.message || 'Something went wrong');
             } else {
@@ -48,6 +67,7 @@ export const useAddProduct = () => {
     return {
         product,
         handleChange,
+        handleImageChange,
         handleSubmit,
         loading,
         error,
